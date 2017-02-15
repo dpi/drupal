1df54005d130709ab883299b1ccbd3f2945c3d33
dpi <pro@danielph.in>
2017-02-15 20:52:54 +0800

    rework

--- core/modules/system/src/Plugin/Block/SystemBrandingBlock.php
+++ core/modules/system/src/Plugin/Block/SystemBrandingBlock.php
@@ -75,40 +75,6 @@ class SystemBrandingBlock extends BlockBase implements ContainerFactoryPluginInt
     // Get the theme.
     $theme = $form_state->get('block_theme');
 
-    // Get permissions.
-    $url_system_theme_settings = new Url('system.theme_settings');
-    $url_system_theme_settings_theme = new Url('system.theme_settings_theme', array('theme' => $theme));
-
-    if ($url_system_theme_settings->access() && $url_system_theme_settings_theme->access()) {
-      // Provide links to the Appearance Settings and Theme Settings pages
-      // if the user has access to administer themes.
-      $site_logo_description = $this->t('Defined on the <a href=":appearance">Appearance Settings</a> or <a href=":theme">Theme Settings</a> page.', array(
-        ':appearance' => $url_system_theme_settings->toString(),
-        ':theme' => $url_system_theme_settings_theme->toString(),
-      ));
-    }
-    else {
-      // Explain that the user does not have access to the Appearance and Theme
-      // Settings pages.
-      $site_logo_description = $this->t('Defined on the Appearance or Theme Settings page. You do not have the appropriate permissions to change the site logo.');
-    }
-    $url_system_site_information_settings = new Url('system.site_information_settings');
-    if ($url_system_site_information_settings->access()) {
-      // Get paths to settings pages.
-      $site_information_url = $url_system_site_information_settings->toString();
-
-      // Provide link to Site Information page if the user has access to
-      // administer site configuration.
-      $site_name_description = $this->t('Defined on the <a href=":information">Site Information</a> page.', array(':information' => $site_information_url));
-      $site_slogan_description = $this->t('Defined on the <a href=":information">Site Information</a> page.', array(':information' => $site_information_url));
-    }
-    else {
-      // Explain that the user does not have access to the Site Information
-      // page.
-      $site_name_description = $this->t('Defined on the Site Information page. You do not have the appropriate permissions to change the site logo.');
-      $site_slogan_description = $this->t('Defined on the Site Information page. You do not have the appropriate permissions to change the site logo.');
-    }
-
     $form['block_branding'] = array(
       '#type' => 'fieldset',
       '#title' => $this->t('Toggle branding elements'),
@@ -117,22 +83,41 @@ class SystemBrandingBlock extends BlockBase implements ContainerFactoryPluginInt
     $form['block_branding']['use_site_logo'] = array(
       '#type' => 'checkbox',
       '#title' => $this->t('Site logo'),
-      '#description' => $site_logo_description,
       '#default_value' => $this->configuration['use_site_logo'],
     );
+
+    // Provide links to the theme settings pages if the user has access.
+    $system_theme_settings_url = Url::fromRoute('system.theme_settings');
+    $block_theme_settings_url = (Url::fromRoute('system.theme_settings_theme'))
+      ->setRouteParameter('theme', $theme);
+    if ($system_theme_settings_url->access() && $block_theme_settings_url->access()) {
+      $form['block_branding']['use_site_logo']['#description'] = $this->t('Defined on the <a href=":appearance">Appearance Settings</a> or <a href=":theme">Theme Settings</a> page.', [
+        ':appearance' => $system_theme_settings_url->toString(),
+        ':theme' => $block_theme_settings_url->toString(),
+      ]);
+    }
 
     $form['block_branding']['use_site_name'] = array(
       '#type' => 'checkbox',
       '#title' => $this->t('Site name'),
-      '#description' => $site_name_description,
       '#default_value' => $this->configuration['use_site_name'],
     );
     $form['block_branding']['use_site_slogan'] = array(
       '#type' => 'checkbox',
       '#title' => $this->t('Site slogan'),
-      '#description' => $site_slogan_description,
       '#default_value' => $this->configuration['use_site_slogan'],
     );
+
+    // Provide link to the site settings page if the user has access.
+    $site_settings_url = Url::fromRoute('system.site_information_settings');
+    if ($site_settings_url->access()) {
+      $description = $this->t('Defined on the <a href=":site-settings">Basic site settings</a> page.', [
+        ':site-settings' => $site_settings_url->toString(),
+      ]);
+      $form['block_branding']['use_site_name']['#description'] = $description;
+      $form['block_branding']['use_site_slogan']['#description'] = $description;
+    }
+
     return $form;
   }
 
